e26cbaaa3d1d1b1dd70802f768f9fd46c646752c
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-12-29 00:44:05 +0530

    updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,30 +5,30 @@ import Tilt from 'react-parallax-tilt';
 import devices from '../../Assets/hero-devices.svg';
 
 const Home = () => {
-    return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            textAlign="center"
-            marginTop="100px"
-        >
-            <Text fontSize="5xl" fontWeight="bold">
-                Software Development Engineer at Jio
-            </Text>
-            <Text fontSize="2xl" style={{marginTop: '30px'}}>
-                I design and code beautifully simple things, and I love what I do.
-            </Text>
-            <Tilt>
-                <img src={avatar} alt="Avatar" style={{ marginTop: '100px' }} />
-            </Tilt>
-            <img
-                src={devices}
-                alt="Devices"
-                style={{ marginTop: '100px', width: '700px' }}
-            />
-        </Box>
-    );
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      textAlign="center"
+      marginTop={{ base: 4, md: 8, lg: 12 }} 
+    >
+      <Text fontSize={{ base: '3xl', md: '5xl' }} fontWeight="bold">
+        Software Development Engineer at Jio
+      </Text>
+      <Text fontSize={{ base: 'xl', md: '2xl' }} marginTop={{ base: 4, md: 6 }}>
+        I design and code beautifully simple things, and I love what I do.
+      </Text>
+      <Tilt>
+        <img src={avatar} alt="Avatar" style={{ marginTop: '100px', width: '100%', maxWidth: '210px' }} />
+      </Tilt>
+      <img
+        src={devices}
+        alt="Devices"
+        style={{ marginTop: '100px', width: '100%', maxWidth: '860px' }}
+      />
+    </Box>
+  );
 };
 
 export default Home;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,60 +1,61 @@
 import React from 'react';
-import { Box, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerBody, useDisclosure, Link} from '@chakra-ui/react';
+import {
+  Box,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerBody,
+  useDisclosure,
+  Flex,
+  Image,
+  Tab,
+  TabList,
+  Tabs,
+} from '@chakra-ui/react';
+import logo from '../../Assets/logo192.png';
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <div>
-      <Box
-        bg='orange'
-        w='100%'
-        p='1.25rem'
-        color='#141C3A'
-        zIndex='auto'
-        boxShadow='none'
-        mt={7}
-        mb={7}
-        display={{ base: 'none', md: 'block' }} 
-      >
-          <Link>Home</Link>
-          <Link>About</Link>
-          <Link>Experience</Link>
-          <Link>Education</Link>
-          <Link>Projects</Link>
-          <Link>Contact</Link>
-      </Box>
-{/* 
-      <Box
-        bg='tomato'
-        w='100%'
-        p='1.25rem'
-        color='white'
-        zIndex='auto'
-        boxShadow='none'
-        mt={7}
-        display={{ base: 'block', md: 'none' }}
-      >
-        <Box mt={4} onClick={onOpen}>
-          Menu
-        </Box>
+    <Flex justifyContent="space-between" alignItems="center" p="1.25rem" bg="white" color="#141C3A" boxShadow="none" mt={7} mb={7}>
+      <Image src={logo} alt="Logo" boxSize="50px" />
 
-        <Drawer isOpen={isOpen} onClose={onClose} placement="right">
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerBody>
-              <Link onClick={onClose}>Home</Link>
-              <Link onClick={onClose}>About</Link>
-              <Link onClick={onClose}>Experience</Link>
-              <Link onClick={onClose}>Education</Link>
-              <Link onClick={onClose}>Projects</Link>
-              <Link onClick={onClose}>Contact</Link>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      </Box> */}
-    </div>
+      <Tabs display={{ base: 'none', md: 'flex' }}>
+        <TabList>
+          <Tab>Home</Tab>
+          <Tab>About</Tab>
+          <Tab>Experience</Tab>
+          <Tab>Education</Tab>
+          <Tab>Projects</Tab>
+          <Tab>Contact</Tab>
+        </TabList>
+      </Tabs>
+
+      <Box display={{ base: 'block', md: 'none' }} onClick={onOpen}>
+        ☰
+      </Box>
+
+      <Drawer isOpen={isOpen} onClose={onClose} placement="right">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <Tabs orientation="vertical">
+              <TabList>
+                <Tab onClick={onClose}>Home</Tab>
+                <Tab onClick={onClose}>About</Tab>
+                <Tab onClick={onClose}>Experience</Tab>
+                <Tab onClick={onClose}>Education</Tab>
+                <Tab onClick={onClose}>Projects</Tab>
+                <Tab onClick={onClose}>Contact</Tab>
+              </TabList>
+            </Tabs>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Flex>
   );
 };
 
