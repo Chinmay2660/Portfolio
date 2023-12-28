16e5e0e380d36c6996b1fd6df0188ebfc43fb59c
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-12-28 22:07:35 +0530

    Added Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import avatar from '../../Assets/avatar.svg';
+import { Box, Text } from '@chakra-ui/react';
+import Tilt from 'react-parallax-tilt';
+import devices from '../../Assets/hero-devices.svg';
+
+const Home = () => {
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            textAlign="center"
+            marginTop="100px"
+        >
+            <Text fontSize="5xl" fontWeight="bold">
+                Software Development Engineer at Jio
+            </Text>
+            <Text fontSize="2xl" style={{marginTop: '20px'}}>
+                I design and code beautifully simple things, and I love what I do.
+            </Text>
+            <Tilt>
+                <img src={avatar} alt="Avatar" style={{ marginTop: '20px' }} />
+            </Tilt>
+            <img
+                src={devices}
+                alt="Devices"
+                style={{ marginTop: '100px', width: '700px' }}
+            />
+        </Box>
+    );
+};
+
+export default Home;
