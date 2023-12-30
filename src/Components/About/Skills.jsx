384cc1a85a3d1c379bf9c32b48621442aa501689
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-12-30 18:47:06 +0530

    Update skills to transparent

--- src/Components/About/Skills.jsx
+++ src/Components/About/Skills.jsx
@@ -3,63 +3,6 @@ import { Box, Text, Flex, VStack, Icon } from '@chakra-ui/react';
 import { FaJava, FaReact, FaNodeJs, FaGithub, FaCode, FaGit, FaDatabase } from 'react-icons/fa';
 import { SiRedux, SiTailwindcss } from 'react-icons/si';
 
-const Skills = () => {
-  return (
-    <Box
-      id="about"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center"
-      position="relative"
-      height="500px"
-      zIndex={1}
-    >
-      <Flex direction="column">
-        <Box
-          p={8}
-          bg="#ffffff"
-          color="black"
-          marginTop="-80%"
-          boxShadow="0 4px 8px rgba(0,0,0,0.1)"
-        >
-          <VStack spacing={4}>
-            <Text fontSize="xl" fontWeight="bold">
-              Tech Stack
-            </Text>
-            {techStack.map((tech) => (
-              <Flex key={tech.name} align="center">
-                <Icon as={tech.icon} boxSize={6} />
-                <Text ml={2}>{tech.name}</Text>
-              </Flex>
-            ))}
-          </VStack>
-        </Box>
-
-        <Box
-          p={8}
-          bg="#ffffff"
-          color="black"
-          marginTop="-80%"
-          boxShadow="0 4px 8px rgba(0,0,0,0.1)"
-        >
-          <VStack spacing={4}>
-            <Text fontSize="xl">
-              Tool Stack
-            </Text>
-            {toolStack.map((tool) => (
-              <Flex key={tool.name} align="center">
-                <Icon as={tool.icon} boxSize={6} />
-                <Text ml={2}>{tool.name}</Text>
-              </Flex>
-            ))}
-          </VStack>
-        </Box>
-      </Flex>
-    </Box>
-  );
-};
-
 const techStack = [
   { name: 'Java', icon: FaJava },
   { name: 'JavaScript', icon: FaReact },
@@ -77,5 +20,69 @@ const toolStack = [
   { name: 'GitHub', icon: FaGithub },
   { name: 'Postman', icon: FaCode },
 ];
+
+const Skills = () => {
+  return (
+    <Box
+      id="skills"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+      position="relative"
+      height="600px"
+      zIndex={1}
+      bg="transparent"
+      marginTop="-200px"
+      paddingBottom={20}
+    >
+      <Flex direction="column">
+        <Box
+          p={8}
+          bg="#ffffff"
+          color="black"
+          border="0.5px solid black"
+          borderRadius="15px 15px 0 0"
+        >
+          <VStack spacing={4}>
+            <Text fontSize="xl" fontWeight="bold">
+              Tech Stack
+            </Text>
+            <Flex align="center" flexDirection="row">
+              {techStack.map((tech) => (
+                <Flex key={tech.name} align="center" flexDirection="column" m={2}>
+                  <Icon as={tech.icon} boxSize={8} />
+                  <Text mt={2}>{tech.name}</Text>
+                </Flex>
+              ))}
+            </Flex>
+          </VStack>
+        </Box>
+
+        <Box
+          p={8}
+          bg="#ffffff"
+          color="black"
+          border="0.5px solid black"
+          borderRadius="0 0 15px 15px"
+        >
+          <VStack spacing={4}>
+            <Text fontSize="xl" fontWeight="bold">
+              Tool Stack
+            </Text>
+            <Flex align="center" flexDirection="row" > 
+              {toolStack.map((tool) => (
+                <Flex key={tool.name} align="center" flexDirection="column" m={2}>
+                  <Icon as={tool.icon} boxSize={8} />
+                  <Text mt={2}>{tool.name}</Text>
+                </Flex>
+              ))}
+            </Flex>
+          </VStack>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
 
 export default Skills;
