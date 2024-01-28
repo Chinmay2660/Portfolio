682e9b361805fc475f410087b1a6bb3e2410f20f
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-01-29 00:25:39 +0530

    fix see more projects position for xl devices

--- src/Components/Projects/ProjectCards.jsx
+++ src/Components/Projects/ProjectCards.jsx
@@ -184,41 +184,45 @@ const ProjectCards = () => {
     const visibleProjects = showAllProjects ? projects : projects.slice(0, 6);
 
     return (
-        <Flex
-            id="projects"
-            flexWrap="wrap"
-            justifyContent="center"
-            alignItems="center"
-            marginTop="70px"
-            marginBottom="70px"
-        >
-            {visibleProjects.map((project) => (
-                <ProjectCard key={project.id} {...project} />
-            ))}
+        <>
+            <Flex
+                id="projects"
+                flexWrap="wrap"
+                justifyContent="center"
+                alignItems="center"
+                marginTop="70px"
+                marginBottom="70px"
+            >
+                {visibleProjects.map((project) => (
+                    <ProjectCard key={project.id} {...project} />
+                ))}
+            </Flex>
             {!showAllProjects && (
-                <Button
-                    onClick={() => setShowAllProjects(true)}
-                    size="lg"
-                    paddingX="5"
-                    paddingY="3"
-                    variant="ghost"
-                    borderColor="#6e07f3"
-                    borderWidth="2px"
-                    borderRadius="20px"
-                    bg='white'
-                    color="#6e07f3"
-                    marginTop='5'
-                    marginBottom='5'
-                    _hover={{
-                        bg: "#6e07f3",
-                        color: "white",
-                    }}
-                >
-                    See more projects
-                    <FaArrowDown style={{ marginLeft: '0.5rem', marginTop: '3px', marginBottom: '3px' }} size="20px" />
-                </Button>
+                <Flex justifyContent="center" marginTop="-60px" marginBottom="25px">
+                    <Button
+                        onClick={() => setShowAllProjects(true)}
+                        size="lg"
+                        paddingX="5"
+                        paddingY="3"
+                        variant="ghost"
+                        borderColor="#6e07f3"
+                        borderWidth="2px"
+                        borderRadius="20px"
+                        bg='white'
+                        color="#6e07f3"
+                        marginTop='5'
+                        marginBottom='5'
+                        _hover={{
+                            bg: "#6e07f3",
+                            color: "white",
+                        }}
+                    >
+                        See more projects
+                        <FaArrowDown style={{ marginLeft: '0.5rem', marginTop: '3px', marginBottom: '3px' }} size="20px" />
+                    </Button>
+                </Flex>
             )}
-        </Flex>
+        </>
     );
 };
 
