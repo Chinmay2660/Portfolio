af532c630ced0d7e426a4b08ffdd2a786474f4ff
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-01-28 23:39:27 +0530

    Change footer data to map function

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -18,10 +18,19 @@ const iconStyle = {
     transition: 'background-color 0.3s ease, color 0.3s ease',
     textDecoration: 'none',
     ":hover": {
-        backgroundColor: 'white', 
-        color: '#6e07f3', 
+        backgroundColor: 'white',
+        color: '#6e07f3',
     }
 }
+
+const iconData = [
+    { link: "https://www.linkedin.com/in/chinmay2660/", icon: FaLinkedin },
+    { link: "https://github.com/Chinmay2660", icon: FaGithub },
+    { link: "mailto:bhoirchinmay2014@gmail.com", icon: FaEnvelope },
+    { link: "https://leetcode.com/Chinmay2660/", icon: SiLeetcode },
+    { link: "https://auth.geeksforgeeks.org/user/chinmay2660", icon: SiGeeksforgeeks },
+    { link: "https://www.hackerrank.com/profile/Chinmay2660", icon: SiHackerrank }
+];
 
 const Footer = () => {
     return (
@@ -33,20 +42,17 @@ const Footer = () => {
             position="relative"
             backgroundColor="#6e07f3"
             color="#ffffff"
-            height={{ base: 'auto', md: '612px' }} 
+            height={{ base: 'auto', md: '612px' }}
             padding={{ base: '-60px', md: '0' }}
         >
-            <Box width="820px" height="500px" display="flex" flexDirection="column" justifyContent="center" marginTop={{ base: '2.5rem', md: '1.5rem' }} marginBottom={{base: '0', md: '0', lg: '0', xl: '0'}}>
-            <Text fontSize={{ base: '2xl', md: '3xl' }} paddingY="2" lineHeight="1.2" >
+            <Box width="820px" height="500px" display="flex" flexDirection="column" justifyContent="center" marginTop={{ base: '2.5rem', md: '1.5rem' }} marginBottom={{ base: '0', md: '0', lg: '0', xl: '0' }}>
+                <Text fontSize={{ base: '2xl', md: '3xl' }} paddingY="2" lineHeight="1.2" >
                     Living, learning, & leveling up <br /> one day at a time.
                 </Text>
                 <Box fontSize="xl" paddingY="2" marginTop="50px" display="flex" justifyContent="center">
-                    <a href="https://www.linkedin.com/in/chinmay2660/" target="_blank" rel="noopener noreferrer" style={iconStyle}><FaLinkedin /></a>
-                    <a href="https://github.com/Chinmay2660" target="_blank" rel="noopener noreferrer" style={iconStyle}><FaGithub /></a>
-                    <a href="mailto:bhoirchinmay2014@gmail.com" style={iconStyle}><FaEnvelope /></a>
-                    <a href="https://leetcode.com/Chinmay2660/" target="_blank" rel="noopener noreferrer" style={iconStyle}><SiLeetcode /></a>
-                    <a href="https://auth.geeksforgeeks.org/user/chinmay2660" target="_blank" rel="noopener noreferrer" style={iconStyle}><SiGeeksforgeeks /></a>
-                    <a href="https://www.hackerrank.com/profile/Chinmay2660" target="_blank" rel="noopener noreferrer" style={iconStyle}><SiHackerrank /></a>
+                    {iconData.map((item, index) => (
+                        <a key={index} href={item.link} target="_blank" rel="noopener noreferrer" style={iconStyle}><item.icon /></a>
+                    ))}
                 </Box>
                 <Text fontSize="xl" paddingY="2" marginTop="1rem">
                     Created by Chinmay @ 2023
