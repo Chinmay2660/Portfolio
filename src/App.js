4d2eff5a1903fff452406e6b40df6b5416e649d1
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-02-03 21:57:19 +0530

    Add contactform component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from './Components/About/Skills';
 import Footer from './Components/Footer/Footer';
 import HireMe from './Components/CTA/HireMe';
 import Projects from './Components/Projects/Projects';
-import GithubBox from './Components/Github/GithubBox'
+// import GithubBox from './Components/Github/GithubBox'
 import Contact from './Components/CTA/Contact';
 import './style.css'
 import ProjectCards from './Components/Projects/ProjectCards';
