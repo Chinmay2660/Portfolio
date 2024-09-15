23a3164968ee16e60895b5b100e8b26394a885f2
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-09-16 01:23:28 +0530

    Fix: padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary lg:mx-auto`}
       >
         <Navbar />
         {children}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
         scrollDirection === "top" ? "bg-opacity-60" : "bg-opacity-40"
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 py-2 border border-gray-700 rounded-lg shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 md:mx-10 py-2 border border-gray-700 rounded-lg shadow-lg lg:px-8">
         <div className="flex justify-between items-center">
           <div className="text-2xl mx-6 font-bold text-white dark:text-gray-300">
             <Link href="/">My Portfolio</Link>
