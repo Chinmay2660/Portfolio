4e4ea49e6acf91ff6a6fc5796bbe5645c22a69ec
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-09-15 17:43:44 +0530

    Fix: contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Contact: FC = () => {
   return (
-    <section className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between bg-primary border border-gray-700 rounded-lg p-8 shadow-lg text-gray-200">
+    <section className="max-w-7xl mx-auto flex flex-col space-x-4 md:flex-row justify-between bg-primary border border-gray-700 rounded-lg p-8 shadow-lg text-gray-200">
       <div className="flex-1 mb-8 md:mb-0">
         <h2 className="text-3xl font-bold">Hit me with an email</h2>
         <p className="mt-4 text-lg">
@@ -10,20 +10,20 @@ const Contact: FC = () => {
         </p>
       </div>
 
-      <div className="flex flex-col space-x-4">
+      <div className="flex flex-col gap-4">
         <a
-          href="mailto:your-email@example.com"
-          className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-300"
+          href="mailto:bhoirchinmay2014@gmail.com"
+          className="text-lg text-gray-200 bg-transparent  shadow-lg border border-gray-700 hover:border-gray-600 px-6 py-3 rounded-lg flex items-center hover:bg-gray-700 hover:text-white transition-all duration-300"
         >
           Send Email
         </a>
 
-        <div className="flex items-center">
+        <div className="flex items-center justify-between gap-2">
           <a
             href="/path-to-your-resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            className="px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white font-semibold rounded-lg transition duration-300"
+            className="text-lg text-gray-200 bg-transparent  shadow-lg border border-gray-700 hover:border-gray-600 px-6 py-3 rounded-lg flex items-center hover:bg-gray-700 hover:text-white transition-all duration-300"
           >
             View Resume
           </a>
@@ -31,7 +31,7 @@ const Contact: FC = () => {
           <a
             href="/path-to-your-resume.pdf"
             download="Chinmay_Bhoir_Resume.pdf"
-            className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white font-semibold rounded-lg flex items-center transition duration-300"
+            className="text-lg text-gray-200 bg-transparent  shadow-lg border border-gray-700 hover:border-gray-600 px-6 py-3 rounded-lg flex items-center hover:bg-gray-700 hover:text-white transition-all duration-300"
           >
             <i className="fas fa-download text-lg"></i>
           </a>
