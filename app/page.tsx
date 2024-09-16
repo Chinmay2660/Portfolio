a8f0e5f73e197ae240f8b86d56e82952c4110d95
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-09-16 10:34:44 +0530

    Fix: changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import ProjectSection from '@/components/ProjectSection';
-import Hero from '@/components/Hero';
-import Contact from '@/components/Contact';
+import ProjectSection from "@/components/ProjectSection";
+import Hero from "@/components/Hero";
+import Contact from "@/components/Contact";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <ProjectSection/>
-      <Contact/>
+      <ProjectSection />
+      <Contact />
     </>
   );
 }
