7e26379c330459aa60c590fcd2366e85bcca7794
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-09-16 11:25:53 +0530

    Fix: margin issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary lg:mx-auto`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary`}
       >
         <Navbar />
         {children}
